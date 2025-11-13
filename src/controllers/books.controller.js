0c2954949d456485b12dfb3b6857ff2355e6605f
Hendrik Aksalu <aksaluhendrik@gmail.com>
2025-11-13 16:40:25 +0200

    should be okay

--- src/controllers/books.controller.js
+++ src/controllers/books.controller.js
@@ -51,8 +51,83 @@ try {
 }
 };
 
-export const createBook = (request, response) => {};
+export const createBook = async (request, response) => {
+    try {
+        const { title, description, thumbnail_url, release_year } = request.body;
 
-export const updateBook = (request, response) => {};
+        const newBook = await prisma.book.create({
+            title,
+            description,
+            thumbnail_url,
+            release_year
+        });
+        
+        response.status(201).json({
+            message: 'Successfully created book',
+            data: newBook
+        });
 
-export const deleteBook = (request, response) => {};
+    } catch (exception) {
+        response.status(500).json({
+            message: 'Something went wrong',
+            error: exception.message
+        });
+    }
+};
+
+export const updateBook = async (request, response) => {
+    try {
+        const {id} = request.params?.id;
+        const { title, description, thumbnail_url, release_year } = request.body;
+        
+        const updatedBook = await prisma.book.update({
+            where: {
+                id: Number(id)
+            },
+            data: {
+                title,
+                description,
+                thumbnail_url,
+                release_year
+            }
+        });
+
+        if (!updatedBook) {
+            response.status(404).json({
+                message: 'Not found'
+            });
+        }
+
+        response.status(200).json({
+            message: 'Successfully updated book',
+            data: updatedBook
+        });
+        
+    } catch (exception) {
+        response.status(500).json({
+            message: 'Something went wrong',
+            error: exception.message
+        });
+    }
+};
+
+export const deleteBook = async (request, response) => {
+    try {
+    const bookId = request.params?.id;
+
+    await prisma.book.delete({
+        where: {
+            id: Number(bookId)
+        }
+    });
+
+    response.status(200).json({
+        message: 'Successfully deleted'
+    });
+} catch (exception) {
+    response.status(500).json({
+        message: 'Something went wrong',
+        error: exception.message
+    });
+}
+};

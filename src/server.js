addc3dfd6e65fa74ce802f8c75632bde44ea3916
Hendrik Aksalu <aksaluhendrik@gmail.com>
2025-11-13 13:22:22 +0200

    first commit

---
+++ src/server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import bookRoutes from './routes/book.route.js';
+
+const app = express();
+const PORT = 3000;
+
+app.get('/welcome', (request, response) => {
+    response.send('Welcome to the server!');
+});
+
+
+app.use('/api/v1', bookRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
